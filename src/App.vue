fcb1752ddc2385bc3f96bc2c3480f87cc2b6c5d2
kumarsrj815 <kumarsrj815@gmail.com>
2019-02-21 23:09:36 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <app-header :maxQuotes="maxQuotes" :quotesCount="quotes.length"></app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                     Click on a quote to delete it!
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
+    export default {
+        data: function(){
+            return {
+                quotes: [],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header
+        },
+        methods: {
+            newQuote(quote){
+                if(this.quotes.length >= this.maxQuotes){
+                    return alert('Please Delete Quote!');
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index){
+                this.quotes.splice(index, 1);
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
